4674378499d88fe3ae71b8acee255e5e0e7919dc
Daniel Gober <dahnygober7@gmail.com>
2023-05-05 11:53:20 -0700

    add addition functionality and grid to store results of addition execution

--- script.js
+++ script.js
@@ -2,36 +2,60 @@ const solveFor = document.querySelector(".solveFor");
 const boxes = document.querySelectorAll(".gridbox");
 const shuffle = document.querySelector(".shuffle");
 const solve = document.querySelector(".solve");
+const solveBoxes = document.querySelectorAll(".gridboxSolve");
+const saveBoxes = document.querySelectorAll(".gridboxSave");
 
+// randomizes numbers across the main grid
 shuffle.addEventListener("click", function () {
   boxes.forEach((box) => {
-    boxNumbers = Math.ceil(Math.random() * 10);
+    let boxNumbers = Math.floor(Math.random() * 11);
     box.textContent = boxNumbers;
     box.style.color = "white";
     console.log(boxNumbers);
     return boxNumbers;
   });
 });
-
-solveFor.addEventListener("click", function () {
+// randomizes numbers in the solution box
+function solutionShuffle() {
   solveNumber = Math.ceil(Math.random() * 1000);
   solveFor.textContent = solveNumber;
-});
+}
+// calls for inital main grid shuffle on load
+solutionShuffle();
+
+// shuffles main grid on click
+solveFor.addEventListener("click", solutionShuffle);
 // WATCH WEB DEV SIMPLIFIED EVENT LISTENERS VIDEO
+// stores clicked numbers in array and pushes to smaller grid
 let numbers = [];
+let solveBoxNumbers = [];
 boxes.forEach((box) => {
   box.addEventListener("click", (e) => {
     const number = e.target.innerText;
     numbers.push(Number(number));
-    console.log(numbers);
+    solveBoxNumbers.push(Number(number));
+    for (let i = 0; i < solveBoxes.length; i++) {
+      solveBoxes[i].textContent = numbers[i];
+    }
+
+    console.log("solveBoxNumbers", solveBoxNumbers);
+    console.log("numbers:", numbers);
     // console.log(event);
     // console.log(box);
   });
 });
 // LOOK UP WHAT THE COMMENTED OUT 'CONST = SUM' IS DOING. I DONT KNOW THIS STUFF.
+// adds together numbers in small grid
 solve.addEventListener("click", () => {
-  //   const sum = numbers.reduce((partialSum, num) => partialSum + num, 0);
-  const sum = numbers[0] + numbers[1];
+  const sum = numbers.reduce((partialSum, num) => partialSum * num, 1);
+  // const sum = numbers[0] + numbers[1];
   console.log(sum);
   numbers = [];
+  solveBoxNumbers = [];
+  for (let i = 0; i < solveBoxes.length; i++) {
+    solveBoxes[i].textContent = numbers[i];
+  }
+  for (let i = 0; i < saveBoxes.length; i++) {
+    saveBoxes[i].textContent = sum;
+  }
 });
