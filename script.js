10da3b5800f0faf0736dde907ad1635f7fa5d25f
Daniel Gober <dahnygober7@gmail.com>
2023-05-05 10:26:14 -0700

    strange commit

--- script.js
+++ script.js
@@ -2,17 +2,14 @@ const solveFor = document.querySelector(".solveFor");
 const boxes = document.querySelectorAll(".gridbox");
 const shuffle = document.querySelector(".shuffle");
 const solve = document.querySelector(".solve");
-let number1;
-let number2;
 
 shuffle.addEventListener("click", function () {
   boxes.forEach((box) => {
     boxNumbers = Math.ceil(Math.random() * 10);
     box.textContent = boxNumbers;
-    delete number1;
-    delete number2;
-    number2 = 0;
     box.style.color = "white";
+    console.log(boxNumbers);
+    return boxNumbers;
   });
 });
 
@@ -20,15 +17,21 @@ solveFor.addEventListener("click", function () {
   solveNumber = Math.ceil(Math.random() * 1000);
   solveFor.textContent = solveNumber;
 });
-
+// WATCH WEB DEV SIMPLIFIED EVENT LISTENERS VIDEO
+let numbers = [];
 boxes.forEach((box) => {
-  box.addEventListener("click", () => {
-    number1 = box.textContent;
-    console.log(number1);
+  box.addEventListener("click", (e) => {
+    const number = e.target.innerText;
+    numbers.push(Number(number));
+    console.log(numbers);
+    // console.log(event);
+    // console.log(box);
   });
 });
-
+// LOOK UP WHAT THE COMMENTED OUT 'CONST = SUM' IS DOING. I DONT KNOW THIS STUFF.
 solve.addEventListener("click", () => {
-  solution = Number(number1 * number2);
-  console.log(solution);
+  //   const sum = numbers.reduce((partialSum, num) => partialSum + num, 0);
+  const sum = numbers[0] + numbers[1];
+  console.log(sum);
+  numbers = [];
 });
