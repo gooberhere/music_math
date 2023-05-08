9714a36e5462a6e05c6dcfdf278b75da630d3ae5
Daniel Gober <dahnygober7@gmail.com>
2023-05-08 16:54:52 -0700

    add addition function and final result CSS manipulation.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const solveFor = document.querySelector(".solveFor");
 const boxes = document.querySelectorAll(".gridbox");
 const shuffle = document.querySelector(".shuffle");
-const solve = document.querySelector(".solve");
-const solveBoxes = document.querySelectorAll(".gridboxSolve");
-const saveBoxes = document.querySelectorAll(".gridboxSave");
+const multiply = document.querySelector(".multiply");
+const add = document.querySelector(".add");
+const multiBoxes = document.querySelectorAll(".gridboxSolve");
+const addBoxes = document.querySelectorAll(".gridboxSave");
+const root = document.querySelector(":root");
+
+let numbers = [];
+let multipliedNumbers = [];
+let multiBoxNumbers = [];
 
 // randomizes numbers across the main grid
 shuffle.addEventListener("click", function () {
@@ -16,29 +22,37 @@ shuffle.addEventListener("click", function () {
   });
 });
 // randomizes numbers in the solution box
-function solutionShuffle() {
+function initialShuffle() {
   solveNumber = Math.ceil(Math.random() * 1000);
   solveFor.textContent = solveNumber;
 }
 // calls for inital main grid shuffle on load
-solutionShuffle();
+initialShuffle();
+
+function reShuffle() {
+  solveNumber = Math.ceil(Math.random() * 1000);
+  solveFor.textContent = solveNumber;
+  numbers = [];
+  multipliedNumbers = [];
+  multiBoxNumbers = [];
+  multiBoxes.textContent = 0;
+}
 
 // shuffles main grid on click
-solveFor.addEventListener("click", solutionShuffle);
+solveFor.addEventListener("click", reShuffle);
 // WATCH WEB DEV SIMPLIFIED EVENT LISTENERS VIDEO
 // stores clicked numbers in array and pushes to smaller grid
-let numbers = [];
-let solveBoxNumbers = [];
+
 boxes.forEach((box) => {
   box.addEventListener("click", (e) => {
     const number = e.target.innerText;
     numbers.push(Number(number));
-    solveBoxNumbers.push(Number(number));
-    for (let i = 0; i < solveBoxes.length; i++) {
-      solveBoxes[i].textContent = numbers[i];
+    multiBoxNumbers.push(Number(number));
+    for (let i = 0; i < multiBoxes.length; i++) {
+      multiBoxes[i].textContent = numbers[i];
     }
 
-    console.log("solveBoxNumbers", solveBoxNumbers);
+    console.log("multiBoxNumbers", multiBoxNumbers);
     console.log("numbers:", numbers);
     // console.log(event);
     // console.log(box);
@@ -46,16 +60,33 @@ boxes.forEach((box) => {
 });
 // LOOK UP WHAT THE COMMENTED OUT 'CONST = SUM' IS DOING. I DONT KNOW THIS STUFF.
 // adds together numbers in small grid
-solve.addEventListener("click", () => {
+multiply.addEventListener("click", () => {
   const sum = numbers.reduce((partialSum, num) => partialSum * num, 1);
   // const sum = numbers[0] + numbers[1];
   console.log(sum);
+  multipliedNumbers.push(Number(sum));
   numbers = [];
-  solveBoxNumbers = [];
-  for (let i = 0; i < solveBoxes.length; i++) {
-    solveBoxes[i].textContent = numbers[i];
+  multiBoxNumbers = [];
+  for (let i = 0; i < multiBoxes.length; i++) {
+    multiBoxes[i].textContent = numbers[i];
   }
-  for (let i = 0; i < saveBoxes.length; i++) {
-    saveBoxes[i].textContent = sum;
+  for (let i = 0; i < addBoxes.length; i++) {
+    addBoxes[i].textContent = multipliedNumbers[i];
+  }
+});
+
+add.addEventListener("click", () => {
+  const newSum = multipliedNumbers.reduce(
+    (partialSum, num) => partialSum + num,
+    0
+  );
+  console.log(newSum);
+  if (Number(newSum) == Number(solveNumber)) {
+    document.body.style.backgroundImage =
+      "linear-gradient(45deg, green, yellow)";
+    root.style.setProperty("--border-color", "white");
+  } else {
+    solutionShuffle();
+    newSum = 0;
   }
 });
