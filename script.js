56580412098138d0733eaf8a8639634f99ca63b7
Daniel Gober <dahnygober7@gmail.com>
2023-05-11 11:56:30 -0700

    add winner and loser messages as well as reset function

--- script.js
+++ script.js
@@ -11,8 +11,12 @@ let numbers = [];
 let multipliedNumbers = [];
 let multiBoxNumbers = [];
 
+// calls for inital main grid and solution shuffle on load
+initialShuffle();
+gridShuffle();
+
 // randomizes numbers across the main grid
-shuffle.addEventListener("click", function () {
+function gridShuffle() {
   boxes.forEach((box) => {
     let boxNumbers = Math.floor(Math.random() * 11);
     box.textContent = boxNumbers;
@@ -20,26 +24,36 @@ shuffle.addEventListener("click", function () {
     console.log(boxNumbers);
     return boxNumbers;
   });
-});
+}
+
 // randomizes numbers in the solution box
 function initialShuffle() {
   solveNumber = Math.ceil(Math.random() * 1000);
   solveFor.textContent = solveNumber;
 }
-// calls for inital main grid shuffle on load
-initialShuffle();
-
-function reShuffle() {
-  solveNumber = Math.ceil(Math.random() * 1000);
-  solveFor.textContent = solveNumber;
+// resets everything
+function reset() {
+  gridShuffle();
+  initialShuffle();
   numbers = [];
   multipliedNumbers = [];
   multiBoxNumbers = [];
-  multiBoxes.textContent = 0;
+  multiBoxes.forEach((multiBox) => {
+    multiBox.textContent = "";
+  });
+  addBoxes.forEach((addBox) => {
+    addBox.textContent = "";
+  });
+  document.body.style.backgroundImage = "linear-gradient(45deg, red, purple)";
+  root.style.setProperty("--border-color", "yellow");
+  document.querySelector(".winMessage").style.display = "none";
+  document.querySelector(".loseMessage").style.display = "none";
 }
 
+shuffle.addEventListener("click", gridShuffle);
+
 // shuffles main grid on click
-solveFor.addEventListener("click", reShuffle);
+solveFor.addEventListener("click", reset);
 // WATCH WEB DEV SIMPLIFIED EVENT LISTENERS VIDEO
 // stores clicked numbers in array and pushes to smaller grid
 
@@ -76,7 +90,7 @@ multiply.addEventListener("click", () => {
 });
 
 add.addEventListener("click", () => {
-  const newSum = multipliedNumbers.reduce(
+  let newSum = multipliedNumbers.reduce(
     (partialSum, num) => partialSum + num,
     0
   );
@@ -85,8 +99,27 @@ add.addEventListener("click", () => {
     document.body.style.backgroundImage =
       "linear-gradient(45deg, green, yellow)";
     root.style.setProperty("--border-color", "white");
+    // document.querySelector(".winMessage").style.display = "block";
+    // solveFor.style.zIndex = "1";
+
+    for (let i = 0; i < boxes.length; i++) {
+      let winner = ["W", "I", "N", "N", "E", "R"];
+      boxes[i].textContent = winner[i];
+    }
+
+    solveFor.textContent = "RESET";
   } else {
-    solutionShuffle();
+    let loser = ["L", "O", "S", "E", "R", ""];
+    for (let i = 0; i < boxes.length; i++) {
+      boxes[i].textContent = loser[i];
+    }
+
+    document.body.style.backgroundImage = "linear-gradient(45deg, orange, red)";
+    root.style.setProperty("--border-color", "white");
+
+    // document.querySelector(".loseMessage").style.display = "block";
+    // solveFor.style.zIndex = "1";
+    solveFor.textContent = "RESET";
     newSum = 0;
   }
 });
